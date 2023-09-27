71ab9ba1d15513176d58be3090dba4cbbb1aa1b6
aybukekcr <aybkkcr@icloud.com>
2023-09-27 14:39:20 +0300

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ export class AppComponent implements OnInit {
     @Input() noteInfo: Note[] = [];
 
     public ngOnInit() {
-
         this.getNotes();
     }
 
--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
   constructor() {
   }
-  // public saveData(key:string,value:string){
-  //   localStorage.setItem(key,value);
-  // }
+
 
 }
